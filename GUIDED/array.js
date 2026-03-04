445ace76d03f0217dfe4acfbdb56eb354c538f5a
Marta817 <martakk668@gmail.com>
2026-03-04 10:33:45 +0700

    memperbarui kodingan

--- GUIDED/array.js
+++ GUIDED/array.js
@@ -3,6 +3,7 @@ kotaDikunjungi.push("Solo");
 kotaDikunjungi.push("Kebumen");
 kotaDikunjungi.pop(); // hapus array dri belakang
 //kota DiKunjungi.reverse();
-kotaDikunjungi.splice(0,1); //hapus array by Index target
-kotaDikunjungi[1]
-console.log(kotaDikunjungi);
+//kotaDikunjungi.splice(0,1); //hapus array by Index target
+//console.log(kota[1]);
+kota[1] = "Bogor", //mengubah array kedua menjadi Bogor
+console.log(kota.slice(0,kota.lenght)); //menampilkan semua array
--- GUIDED/main.js
+++ GUIDED/main.js
@@ -1,27 +1,27 @@
-function ganjilGenap(input){
-    if(input % 2 === 0){
-        return true;
-    }
+// function ganjilGenap(input){
+//     if(input % 2 === 0){
+//         return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-function absoultValue(input){
-if (input < 0){
-    return -input;
-}
-}
+// function absoultValue(input){
+// if (input < 0){
+//     return -input;
+// }
+// }
 
-let apakahGenap = absoultValue(145);
-console.log(apakahGenap);
+// let apakahGenap = absoultValue(145);
+// console.log(apakahGenap);
 
-let nama = "Marta";
-console.log(`Helo, nama`);
+// let nama = "Marta";
+// console.log(`Helo, nama`);
 
-const user = {
-    firstName: "Marta",
-    lastName: "Safitri",
-    age: 19
-};
+// const user = {
+//     firstName: "Marta",
+//     lastName: "Safitri",
+//     age: 19
+// };
 
-console.log(user["age"]);
+// console.log(user["age"]);
