a6049d293d11536fe6475ed43cbec16c5047ab21
Marta817 <martakk668@gmail.com>
2026-03-16 07:40:59 +0700

    menambahkan tugas week 3

--- 02_Pemrograman_JavaScript/TP_04_AutomataDanTable-DrivenConstruction/index.js
+++ 02_Pemrograman_JavaScript/TP_04_AutomataDanTable-DrivenConstruction/index.js
@@ -7,27 +7,40 @@ const hk = document.getElementById("hk");
 const btnBesar = document.getElementById("huruf-besar");
 const btnKecil = document.getElementById("huruf-kecil");
 
+const btnTerang = document.getElementById("tombol-terang");
+const btnGelap = document.getElementById("tombol-gelap");
+
+// hitung huruf
 editor.addEventListener("input", function () {
 
     let text = editor.value;
 
-    // jumlah semua karakter
     hf.textContent = text.length;
 
-    // hitung huruf besar
     let besar = (text.match(/[A-Z]/g) || []).length;
 
-    // huruf kecil = semua karakter - huruf besar
     let kecil = text.length - besar;
 
     hb.textContent = besar;
     hk.textContent = kecil;
 });
 
+// ubah huruf besar
 btnBesar.addEventListener("click", function () {
     editor.value = editor.value.toUpperCase();
 });
 
+// ubah huruf kecil
 btnKecil.addEventListener("click", function () {
     editor.value = editor.value.toLowerCase();
+});
+
+// MODE GELAP
+btnGelap.addEventListener("click", function () {
+    document.documentElement.classList.add("mode-gelap");
+});
+
+// MODE TERANG
+btnTerang.addEventListener("click", function () {
+    document.documentElement.classList.remove("mode-gelap");
 });
